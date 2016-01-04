9f74c27abbcff2b33d9486aaea64bde39d7a246a
Chelsea Johnson <cheljoh@gmail.com>
2016-01-04 14:28:39 -0700

    iter 1

--- lib/server.rb
+++ lib/server.rb
@@ -2,37 +2,48 @@ require 'socket'
 require 'pry'
 
 class Server
-  attr_reader :client
+  attr_accessor :client, :request_counter
+  attr_reader :request_lines
 
   def initialize
-    #request
+    @client = TCPServer.new(9292).accept
     @request_counter = 0
+    @request_lines = []
+    while line = client.gets and !line.chomp.empty? #assign while, loop through
+      request_lines << line.chomp
+    end
   end
 
-   def request
-     tcp_server = TCPServer.new(9292)
-     @client = tcp_server.accept
-     request_lines = []
-     while line = client.gets and !line.chomp.empty? #assign while, loop through
-       request_lines << line.chomp
-     end
-     client.puts "<pre>\nVerb: #{request_lines[0].split[0]}\nPath: #{request_lines[0].split[1]}\nProtocol: #{request_lines[0].split[2]}"\
-     "Host: #{request_lines[1].split[1][0..8]}\nPort: #{request_lines[1].split[1][10..13]}\nOrigin: not sure what to put here\n"\
-     "Accept: #{request_lines[4].split[1]}\n #{request_lines}</pre>"
-     request_lines
-   end
+ #   def request
+ #     request_counter += 1
+ # #     client.puts "<pre>
+ # # Verb: #{request_lines[0].split[0]}
+ # # Path: #{request_lines[0].split[1]}
+ # # Protocol: #{request_lines[0].split[2]}
+ # # Host: #{request_lines[1].split[1][0..8]}
+ # # Port: #{request_lines[1].split[1][10..13]}
+ # # Origin: #{request_lines[1].split[1][0..8]}
+ # # Accept: #{request_lines[4].split[1]}
+ # #     </pre>"
+ #     request_lines
+ #   end
 
    def diagnostics
      request_lines = request
-     client.puts "<pre>\nVerb: #{request_lines[0].split[0]}\nPath: #{request_lines[0].split[1]}\nProtocol: #{request_lines[0].split[2]}"\
-     "Host: #{request_lines[1].split[1][0..8]}\nPort: #{request_lines[1].split[1][10..13]}\nOrigin: not sure what to put here\n"\
-     "Accept: #{request_lines[4].split[1]}\n #{request_lines}</pre>"
+     client.puts "<pre>
+ Verb: #{request_lines[0].split[0]}
+ Path: #{request_lines[0].split[1]}
+ Protocol: #{request_lines[0].split[2]}
+ Host: #{request_lines[1].split[1][0..8]}
+ Port: #{request_lines[1].split[1][10..13]}
+ Origin: #{request_lines[1].split[1][0..8]}
+ Accept: #{request_lines[4].split[1]}
+     </pre>"
    end
 
-  def message
-    message = "Hello, World! (#{@request_counter})"
-    @request_counter += 1
-    client.puts message
+  def respond
+    output_message = "<pre> Hello, World! (#{request_counter}) </pre>"
+    client.puts output_message
   end
 
   def close
@@ -40,13 +51,15 @@ class Server
   end
 end
 
+# binding.pry
 server = Server.new
-server.request
-#server.diagnostics
-server.message
-server.message
-server.message
-server.close
+server.respond
+# server.request
+# server.diagnostics
+# server.message
+# server.request
+# server.message
+# server.close
 
 
 # class Server
